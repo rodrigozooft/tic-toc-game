e0c9cb7cae069143a9730c3bba4da5de8a931ad3
Rod Morgado <rod@yourdeepdata.com>
2024-07-24 18:06:00 -0700

    adding useState to handle edit button state

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,11 +1,23 @@
+import { useState } from 'react';
+
 export default function Player({name, symbol}) {
+  const [ isEditing, setIsEditing ] = useState(false);
+
+  function editButtonClickHandler() {
+    setIsEditing(!isEditing);
+  }
+
   return(
     <li>
       <span className="player">
-        <span className="player-name">{name}</span>
+      {isEditing ? 
+          <input type='text' required value={name}></input>
+         :
+          <span className="player-name">{name}</span>
+      }
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button>Edit</button>
+      <button onClick={editButtonClickHandler}>{isEditing ? 'Save' : 'Edit'}</button>
     </li>
   );
 }
