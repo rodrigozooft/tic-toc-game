89dc9ed43cf7fc4871f2a1830c5af0ff72211490
Rod Morgado <rod@yourdeepdata.com>
2024-08-05 16:38:21 -0700

    implementation of two way binding.

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,19 +1,25 @@
 import { useState } from 'react';
 
-export default function Player({name, symbol}) {
+export default function Player({ initialName, symbol}) {
+  const [playerName, setPlayerName] = useState(initialName);
   const [ isEditing, setIsEditing ] = useState(false);
 
   function editButtonClickHandler() {
-    setIsEditing(!isEditing);
+    setIsEditing(() => !isEditing);
+  }
+
+  function handleNameChange(event) {
+    console.log(event)
+    setPlayerName(event.target.value);
   }
 
   return(
     <li>
       <span className="player">
       {isEditing ? 
-          <input type='text' required value={name}></input>
+          <input type='text' required value={playerName} onChange={handleNameChange}></input>
          :
-          <span className="player-name">{name}</span>
+          <span className="player-name">{playerName}</span>
       }
         <span className="player-symbol">{symbol}</span>
       </span>
